b3fd475cb3d9bd1f60a7baef35b2efac73546a42
Deekshitha <deekshithaa174@gmail.com>
2026-01-26 22:46:34 +0530

    Create 1041-AvailableCapturesForRook

---
+++ 1041-AvailableCapturesForRook/1041-AvailableCapturesForRook.java
@@ -0,0 +1,49 @@
+// Last updated: 1/26/2026, 5:16:40 PM
+class Solution {
+    public int numRookCaptures(char[][] board) {
+        int attack=0;
+        int row=0, col=0;
+        for(int i=0;i<=7;i++){
+            for(int j=0;j<=7;j++){
+                if(board[i][j]=='R'){
+                    row=i;
+                    col=j;
+                    break;
+                }
+            }
+        }
+        int r=row;int c=col;
+        while(r>=0 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            r--;
+        }
+        r=row;c=col;
+        while(r<=7 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            r++;
+        }
+        r=row;c=col;
+        while(c>=0 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            c--;
+        }
+        r=row;c=col;
+        while(c<=7 && board[r][c]!='B'){
+            if(board[r][c]=='p'){
+                attack++;
+                break;
+            }
+            c++;
+        }
+        return attack;
+    }
+}
